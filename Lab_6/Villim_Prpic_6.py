1a533ca1742df84fe1a5f2682257f6594caa38b9
Fluntin <112729558+Fluntin@users.noreply.github.com>
2023-04-01 22:19:09 +0200

    Try

--- Lab_6/Villim_Prpic_6.py
+++ Lab_6/Villim_Prpic_6.py
@@ -1,115 +1,181 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr 25 10:17:02 2022
+# Villim Prpic
+# 2023-03-30
+# DD100N
 
-@author: villi
-"""
+#Programmet läser in husdjur från en textfil.
+#Det ger användaren flera valmöjligheter.
+#För varje husdjur kan användaren välja att klappa det eller mata det.
+#Efter en action ändras siffrorna.
+#Ändringarna sparas i textfilen.
+#En användare kan välja att visa husdjursnamn och status för varje behov.
+#--------------------------------------------------------------------
 
+import os
 import os.path
 
-class Pet:
-    def init(self, name, hunger=0, affection=0):
+#--------------------------------------------------------------------
+# clear terminal screen
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+#--------------------------------------------------------------------
+# 1. Skapa en klass som representerar ett husdjur som har minst tre attribut (t.ex. namn, hunger och klappbehov). 
+#    Du väljer själva vilken datatyp du vill att attributen ska ha. Definiera en konstruktor för klassen.
+#
+# 2. Definiera:
+#    en __str__-metod (eller/och  en __repr__-metod) och
+#    en __lt__-metod där du själv får välja vilket/vilka attribut du vill sortera efter.
+#
+# 3. Definiera minst två metoder till (t.ex. klappa() och mata()). 
+#    Se till att dessa metoderna ändrar värdet attributen på något sätt.
+#--------------------------------------------------------------------
+class Husdjur :
+    
+    #Denna metod är en konstruktor för klassen Husdjur. Den tar emot tre inparametrar.
+    #Metoden sätter dessa värden som egenskaper för den aktuella instansen av klassen Husdjur.
+    def __init__(self, name, hunger=0, klappbehov=0):
         self.name = name
         self.hunger = hunger
-        self.affection = affection
+        self.klappbehov = klappbehov
 
+    #Denna metod för klassen Husdjur som definierar hur en instans av klassen ska representeras
+    #som en sträng när den skrivs ut med funktionen print()
+    #Metoden tar inte emot några inparametrar utöver self
+    #Metoden returnerar en sträng som innehåller information om husdjuret
     def __str__(self):
-        return f"{self.name}, Hunger: {self.hunger}, Affection: {self.affection}"
+        return f"{self.name}, Hunger: {self.hunger}, Klappbehov: {self.klappbehov}"
 
+    #Denna metod för klassen Husdjur som definierar hur två instanser av klassen ska jämföras när de sorteras.
+    #Tar emot två inparametrar self, som är en referens till den första instansen och other som är referens till den andra instansen av klassen som ska jämföras.
+    #Returnerar en boolesk värdering som indikerar resultatet av jämförelsen.
     def __lt__(self, other):
         return self.name < other.name
 
-    def pet(self):
-        self.affection += 1
-        print(f"You pet {self.name}.")
+    def klappa(self):
+        self.klappbehov -= 1
+        print(f"Du klappar {self.name}.")
 
-    def feed(self):
+    def mata(self):
         self.hunger -= 1
-        print(f"You feed {self.name}.")
-        
-def load_pets_from_file(file_path):
-    pets = []
-    if os.path.isfile(file_path):
-        with open(file_path, "r") as file:
+        print(f"Du mattar {self.name}.")
+
+#-------------------------------------------------------------------- 
+def load_husdjur():
+    #Dina husdjurs-objekt ska vara lagrade i en lista.
+    husdjur_lista = list()
+    
+    husdjurfil = "pets.txt"  # Filnamnet för textfilen där resultaten sparas
+    
+    try:
+        with open(husdjurfil, "r") as file:
             for line in file:
                 pet_info = line.strip().split(",")
-                pet = Pet(pet_info[0], int(pet_info[1]), int(pet_info[2]))
-                pets.append(pet)
-    else:
+                pet = Husdjur(pet_info[0], int(pet_info[1]), int(pet_info[2]))
+                husdjur_lista.append(pet)
+                
+    except FileNotFoundError:
+        #Om file pets.txt inte existerar.
         print("Error: File not found.")
-    return pets
-
-def save_pets_to_file(file_path, pets):
+        husdjur_lista = list()
+        
+    return husdjur_lista
+#--------------------------------------------------------------------
+def save_husdjur(file_path, pets):
     with open(file_path, "w") as file:
         for pet in pets:
-            file.write(f"{pet.name},{pet.hunger},{pet.affection}\n")
-def list_pets(pets):
+            file.write(f"{pet.name},{pet.hunger},{pet.klappbehov}\n")
+#--------------------------------------------------------------------            
+def list_husdjur(pets):
     if not pets:
-        print("No pets found.")
+        print("Inga husdjur i lista.")
     else:
+        pets.sort()
         for i, pet in enumerate(pets):
             print(f"{i+1}. {pet}")
-def find_pet(pets, name):
+#--------------------------------------------------------------------
+def hitta_husdjur(pets, name):
     for pet in pets:
         if pet.name.lower() == name.lower():
             return pet
     return None
-
+#--------------------------------------------------------------------
 def main():
+#--------------------------------------------------------------------
+#   4. Skapa ett objekt av klassen och prova att det går att skriva ut husdjurens värden med print().
+#    print("Prova att det går att skriva ut husdjurens värden med print():")
+#   husdjur0=Husdjur("Selma",3,3)
+#    print(husdjur0)
+#    print("Nice!\n")
+#--------------------------------------------------------------------
+#   5. Skapa nu flera olika husdjur och lägg dem i en lista. 
+#      Sortera listan och  skriv en slinga som skriver ut alla husdjur.
+#
+#    husdjur_lista=list()
+#    husdjur_lista.append(husdjur0)
+#    
+#    husdjur1 = Husdjur("Pooper", hunger=3, klappbehov=3)
+#    husdjur_lista.append(husdjur1)
+#    
+#    husdjur2 = Husdjur("Hegel", hunger=1, klappbehov=2)
+#    husdjur_lista.append(husdjur2)
+#    
+#    husdjur3 = Husdjur("Marx", hunger=100, klappbehov=0)
+#    husdjur_lista.append(husdjur3)
+#    
+#    husdjur_lista.sort()
+#    for husdjur in husdjur_lista:
+#        print(husdjur)
+#     
+#--------------------------------------------------------------------
     file_path = "pets.txt"
-    pets = load_pets_from_file(file_path)
+    pets = load_husdjur()
+    print("\n")
+    print("Välkommen till PetRobo!")
 
     while True:
-        print("\nWelcome to PetRobo! What can I assist you with?")
-        print("1. List pets and their status")
-        print("2. Find pet")
-        print("3. Pet pet")
-        print("4. Feed pet")
-        print("5. Exit")
+        print("\n")
+        print("Vad kan jag stå till tjänst med?")
+        print("1. Lista husdjur och deras status")
+        print("2. Leta upp djur")
+        print("3. Avsluta")
 
-        choice = input("> ")
+        choice = input()
+        print("\n")
 
         if choice == "1":
-            list_pets(pets)
+            list_husdjur(pets)
+            
         elif choice == "2":
-            name = input("Which pet do you want me to find? ")
-            pet = find_pet(pets, name)
+            name = input("Vilket djur vill du att jag letar upp? ")
+            pet = hitta_husdjur(pets, name)
             if pet:
-                print(f"Found {pet.name} under the couch.")
+                print(f"Hittade  {pet.name} under soffan, vad ska jag göra nu?")
                 while True:
-                    print("What should I do now?")
-                    print("1. Pet pet")
-                    print("2. Feed pet")
-                    print("3. Back to main menu")
-                    sub_choice = input("> ")
-                    if sub_choice == "1":
-                        pet.pet()
-                    elif sub_choice == "2":
-                        pet.feed()
-                    elif sub_choice == "3":
+                    print(f"1. Klappa {pet.name}")
+                    print(f"2. Mata {pet.name}")
+                    print("3. Tillbaka till huvudmenyn")
+                    action = input()
+                    if action == "1":
+                        print(f"Klappar {pet.name}")
+                        pet.klappa()
+                    elif action == "2":
+                        print(f"Mattar {pet.name}")
+                        pet.mata()
+                    elif action == "3":
                         break
                     else:
-                        print("Invalid choice.")
+                        print("Invalid val.")
             else:
-                print(f"Sorry, I could not find {name}.")
+                print(f"Tyvärr kunde jag inte hitta {name}.")
+
+
         elif choice == "3":
-            name = input("Which pet do you want me to pet? ")
-            pet = find_pet(pets, name)
-            if pet:
-                pet.pet()
-            else:
-                print(f"Sorry, I could not find {name}.")
-        elif choice == "4":
-            name = input("Which pet do you want me to feed? ")
-            pet = find_pet(pets, name)
-            if pet:
-                pet.feed()
-            else:
-                print(f"Sorry, I could not find {name}.")
-        elif choice == "5":
-            save_pets_to_file(file_path, pets)
-            print("Program terminated. Thank you for using PetRobo!")
+            #Programmet ska skriva till samma textfil en gång, strax innan det att programmet avslutas.
+            save_husdjur(file_path, pets)
+            print("Programmet avslutas. Tack för att du använder PetRobo!")
+            break
         else:
-            print("Invalid choice, please try again.\n")
+            print("Invalid val")
+            print("\n")
 
 main()
